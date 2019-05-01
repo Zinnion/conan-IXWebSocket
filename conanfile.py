f9803b3ba12305006fc39f51e6b59e59d356f46a
Mauro Delazeri <maurodelazeri@gmail.com>
2019-05-01 17:33:44 -0400

    update

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,7 @@ import os
 
 class NanomsgConan(ConanFile):
     name = "IXWebSocket"
-    version = "1.3.9"
+    version = "1.4.0"
     description = "WebSocket client/server"
     topics = ("conan", "IXWebSocket", "communication", "socket", "websocket")
     url = "https://github.com/zinnion/conan-IXWebSocket"
